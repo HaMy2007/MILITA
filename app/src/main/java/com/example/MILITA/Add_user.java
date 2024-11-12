2e06763124d8fa44854cb58e712f313bed98964a
Nguyen Ha My <52200066@student.tdtu.edu.vn>
2024-11-12 15:25:10 +0700

    change cer

--- app/src/main/java/com/example/MILITA/Add_user.java
+++ app/src/main/java/com/example/MILITA/Add_user.java
@@ -1,6 +1,9 @@
 package com.example.MILITA;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,8 +13,10 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.MILITA.R;
 
-public class Add_user extends AppCompatActivity {
+import java.util.Calendar;
 
+public class Add_user extends AppCompatActivity {
+    EditText et_birthday;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +27,26 @@ public class Add_user extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        et_birthday = findViewById(R.id.et_birthday);
 
+        et_birthday.setOnClickListener(view -> showDatePickerDialog());
+    }
 
+    private void showDatePickerDialog() {
+        final Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                Add_user.this,
+                (DatePicker view, int selectedYear, int selectedMonth, int selectedDay) -> {
+                    String date = selectedDay + "/" + (selectedMonth + 1) + "/" + selectedYear;
+                    et_birthday.setText(date);
+                },
+                year, month, day
+        );
+
+        datePickerDialog.show();
     }
 }
--- app/src/main/java/com/example/MILITA/Edit_certificate_infomation.java
+++ app/src/main/java/com/example/MILITA/Edit_certificate_infomation.java
@@ -1,6 +1,9 @@
 package com.example.MILITA;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +11,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Edit_certificate_infomation extends AppCompatActivity {
+import java.util.Calendar;
 
+public class Edit_certificate_infomation extends AppCompatActivity {
+    EditText et_session;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +25,25 @@ public class Edit_certificate_infomation extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        et_session = findViewById(R.id.et_session);
+        et_session.setOnClickListener(view -> showDatePickerDialog());
+    }
+
+    private void showDatePickerDialog() {
+        final Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                Edit_certificate_infomation.this,
+                (DatePicker view, int selectedYear, int selectedMonth, int selectedDay) -> {
+                    String date = selectedDay + "/" + (selectedMonth + 1) + "/" + selectedYear;
+                    et_session.setText(date);
+                },
+                year, month, day
+        );
+
+        datePickerDialog.show();
     }
 }
